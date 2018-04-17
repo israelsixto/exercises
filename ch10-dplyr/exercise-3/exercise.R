575b57307bcc173b6a969123761956270b637d37
Israel Sixto-Sanchez <israelsixtosanchez@gmail.com>
2018-04-17 15:27:23 -0700

    Add completed information

--- ch10-dplyr/exercise-3/exercise.R
+++ ch10-dplyr/exercise-3/exercise.R
@@ -11,12 +11,21 @@ library(fueleconomy)
 
 # Which 2015 Acura model has the best hwy MGH? (Use dplyr, but without method
 # chaining or pipes--use temporary variables!)
-
+make_car <- filter(vehicles, make == "Acura")
+year_car <- filter(make_car, year == 2015)
+best_car <- filter(year_car, hwy == max(hwy))
+bestest_car <- select(best_car, model)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr, nesting functions)
+make_car <-
 
+bestest_car <- select(best_car, model)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr and the pipe operator)
+first_car <-filter(vehicles, make == "Acura") %>% 
+  filter( year == 2015) %>% 
+  filter(hwy == max(hwy))
+bestestest_car <- select(first_car, model)
 
 
 
